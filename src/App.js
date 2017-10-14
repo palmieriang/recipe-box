c2e19dc60bf83b0d7fd8eff5083f9fbc4b348971
Angelo Palmieri <palmieri.ang@gmail.com>
2017-10-14 23:33:25 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react'
+import './App.css'
+import Accordion from 'react-bootstrap/lib/Accordion'
+import Panel from 'react-bootstrap/lib/Panel'
+import Button from 'react-bootstrap/lib/Button'
+import ButtonToolbar from 'react-bootstrap/lib/ButtonToolbar'
+import Modal from 'react-bootstrap/lib/Modal'
+import FormGroup from 'react-bootstrap/lib/FormGroup'
+import ControlLabel from 'react-bootstrap/lib/ControlLabel'
+import FormControl from 'react-bootstrap/lib/FormControl'
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+      test
+      </div>
+    );
+  }
+}
+
+export default App;
