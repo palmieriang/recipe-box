9732b6e437c5471681d09d8f0e9624eeaf5e160a
Angelo <angelo.palmieri@rufusleonard.com>
2019-06-24 13:39:55 +0100

    new RecipesList component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,6 @@
 import React, {Component} from 'react'
 import './App.css'
-import Accordion from 'react-bootstrap/lib/Accordion'
-import Panel from 'react-bootstrap/lib/Panel'
 import Button from 'react-bootstrap/lib/Button'
-import ButtonToolbar from 'react-bootstrap/lib/ButtonToolbar'
 import Modal from 'react-bootstrap/lib/Modal'
 import FormGroup from 'react-bootstrap/lib/FormGroup'
 import ControlLabel from 'react-bootstrap/lib/ControlLabel'
@@ -11,48 +8,7 @@ import FormControl from 'react-bootstrap/lib/FormControl'
 import uuidv4 from 'uuid/v4'
 import axios from 'axios'
 
-import heartempty from './images/heart-empty2.png'
-import heartfull from './images/heart-full2.png'
-
-class RecipesList extends Component {
-  render() {
-    const {recipes, changeFavourite, open, deleteRecipe} = this.props
-
-    return (
-      <Accordion>
-        {recipes.map((recipe, index) => (
-          <Panel className="recipe-box" header={
-            <div className="recipe-header">
-              {recipe.img && <div className="recipe-image">
-                <img src={recipe.img} alt={recipe.recipeName} width="100%"/>
-              </div>}
-              <h2 className="recipe-name">{recipe.recipeName} <img className="icon-favourite" src={recipe.favourite ? heartfull : heartempty} alt='heart' onClick={() => changeFavourite(index)} /></h2>
-            </div>
-          } eventKey={index} key={index}>
-            <div className="recipe-body">
-              <div className="recipe-ingredients">
-                <h3>Ingredients</h3>
-                <ul>
-                  {recipe.ingredients.split(',').map((ingredient, index) => (
-                    <li key={index}>{ingredient}</li>
-                  ))}
-                </ul>
-              </div>
-              <div className="recipe-method">
-                <h3>Method</h3>
-                <p>{recipe.method}</p>
-              </div>
-            </div>
-            <ButtonToolbar className="recipe-btn">
-              <Button bsStyle="info" onClick={() => open(recipe.id)}>Edit Recipe</Button>
-              <Button bsStyle="danger" onClick={() => deleteRecipe(index)}>Delete Recipe</Button>
-            </ButtonToolbar>
-          </Panel>
-        ))}
-      </Accordion>
-    )
-  }
-}
+import RecipesList from './components/RecipesList'
 
 class App extends Component {
   constructor () {
---
+++ src/components/RecipesList.js
@@ -0,0 +1,50 @@
+import React, {Component} from 'react'
+import Accordion from 'react-bootstrap/lib/Accordion'
+import Panel from 'react-bootstrap/lib/Panel'
+import ButtonToolbar from 'react-bootstrap/lib/ButtonToolbar'
+import Button from 'react-bootstrap/lib/Button'
+
+import heartempty from '../images/heart-empty2.png'
+import heartfull from '../images/heart-full2.png'
+
+class RecipesList extends Component {
+  render() {
+    const {recipes, changeFavourite, open, deleteRecipe} = this.props
+
+    return (
+      <Accordion>
+        {recipes.map((recipe, index) => (
+          <Panel className="recipe-box" header={
+            <div className="recipe-header">
+            {recipe.img && <div className="recipe-image">
+                <img src={recipe.img} alt={recipe.recipeName} width="100%"/>
+            </div>}
+            <h2 className="recipe-name">{recipe.recipeName} <img className="icon-favourite" src={recipe.favourite ? heartfull : heartempty} alt='heart' onClick={() => changeFavourite(index)} /></h2>
+            </div>
+        } eventKey={index} key={index}>
+            <div className="recipe-body">
+              <div className="recipe-ingredients">
+                <h3>Ingredients</h3>
+                <ul>
+                  {recipe.ingredients.split(',').map((ingredient, index) => (
+                      <li key={index}>{ingredient}</li>
+                  ))}
+                </ul>
+              </div>
+              <div className="recipe-method">
+                <h3>Method</h3>
+                <p>{recipe.method}</p>
+              </div>
+            </div>
+            <ButtonToolbar className="recipe-btn">
+              <Button bsStyle="info" onClick={() => open(recipe.id)}>Edit Recipe</Button>
+              <Button bsStyle="danger" onClick={() => deleteRecipe(index)}>Delete Recipe</Button>
+            </ButtonToolbar>
+          </Panel>
+          ))}
+      </Accordion>
+    )
+  }
+}
+
+export default RecipesList
