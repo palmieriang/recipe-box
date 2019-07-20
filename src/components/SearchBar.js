f7db7dc09ff2ca5053eb683cf8d5bde42ac8c613
Angelo Palmieri <palmieri.ang@gmail.com>
2019-07-20 23:38:50 +0100

    SearchBar input focused

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,13 +1,36 @@
 import React, {Component} from 'react'
 
 class SearchBar extends Component {
+    constructor () {
+        super();
+        this.state = {
+            focused: false
+        }
+    }
+
+    onFocus = () => {
+        this.setState({
+            focused: true
+        })
+    }
+
+    onBlur = (event) => {
+        const inputContent = event.target.value
+
+        if (inputContent.length <= 0) {
+            this.setState({
+                focused: false
+            })
+        }
+    }
+
     render() {
         const {searchBar, searchValue, onChange} = this.props
 
         return (
-            <form className="search-bar" onSubmit={searchBar}>
+            <form className={'search-bar' + (this.state.focused ? ' focused' : '') } onSubmit={searchBar}>
                 <label htmlFor="search-recipe">Search recipe</label>
-                <input id="search-recipe" type="text" placeholder="Search" value={searchValue} onChange={onChange} />
+                <input id="search-recipe" type="text" placeholder="" value={searchValue} onFocus={this.onFocus} onBlur={(event) => this.onBlur(event)} onChange={onChange} />
             </form>
         )
     }
