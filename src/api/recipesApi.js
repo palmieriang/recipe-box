c0ca75d43677baafaaca7f5fc915f176e7121e74
Angelo Palmieri <palmieri.ang@gmail.com>
2019-07-27 07:21:45 +0100

    Added baseUrl.js

--- src/api/recipesApi.js
+++ src/api/recipesApi.js
@@ -1,36 +1,39 @@
 import axios from 'axios'
+import getBaseUrl from './baseUrl'
+
+const baseUrl = getBaseUrl();
 
 export function getRecipe() {
-  return get()
+  return get('recipes')
 }
 
 export function addRecipe(recipe) {
-  return add(recipe)
+  return add(recipe, 'recipes')
 }
 
 export function modifyRecipe(currentRecipe) {
-  return modify(currentRecipe)
+  return modify(currentRecipe, 'recipes')
 }
 
 export function removeRecipe(recipe) {
-  return remove(recipe)
+  return remove(recipe, 'recipes')
 }
 
 export function searchRecipe(query) {
-  return search(query)
+  return search(query, 'recipes')
 }
 
-export function sortRecipes() {
-  return sort()
+export function sortRecipes(order) {
+  return sort(order, 'recipes')
 }
 
-function get() {
-  return axios.get('http://localhost:9627/recipes')
+function get(url) {
+  return axios.get(baseUrl + url)
     .then(onSuccess, onError)
 }
 
-function add(currentRecipe) {
-  return axios.post('http://localhost:9627/recipes',{
+function add(currentRecipe, url) {
+  return axios.post(baseUrl + url, {
       id: currentRecipe.id,
       recipeName: currentRecipe.recipeName,
       img: currentRecipe.img,
@@ -41,8 +44,8 @@ function add(currentRecipe) {
     .then(onSuccess, onError)
 }
 
-function modify(recipe) {
-  return axios.put(`http://localhost:9627/recipes/${recipe.id}`,{
+function modify(recipe, url) {
+  return axios.put(baseUrl + url + `/${recipe.id}`,{
       id: recipe.id,
       recipeName: recipe.recipeName,
       img: recipe.img,
@@ -53,18 +56,18 @@ function modify(recipe) {
     .then(onSuccess, onError)
 }
 
-function remove(id) {
-  return axios.delete(`http://localhost:9627/recipes/${id}`)
+function remove(id, url) {
+  return axios.delete(baseUrl + url + `/${id}`)
     .then(onSuccess, onError)
 }
 
-function search(query) {
-  return axios.get(`http://localhost:9627/recipes?q=${query}`)
+function search(query, url) {
+  return axios.get(baseUrl + url + `?q=${query}`)
     .then(onSuccess, onError)
 }
 
-function sort() {
-  return axios.get('http://localhost:9627/recipes?_sort=recipeName&_order=asc')
+function sort(order, url) {
+  return axios.get(baseUrl + url + `?_sort=recipeName&_order=${order}`)
   .then(onSuccess, onError)
 }
 
